ec50d1f4341c924089963997a155cd15fd729401
Glory Adaeze Glory <daeze7@gmail.com>
2022-11-05 00:16:36 -0700

    This is the second commit

--- musicapp/models.py
+++ musicapp/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    age = models.IntegerField()
+
+
+    def __str__(self):
+        return self.first_name
+ 
+
+
+class Song(models.Model):
+    title = models.CharField(max_length=50)
+    song_released_date = models.DateField(default= datetime.today)
+    likes = models.CharField(max_length=50)
+    artiste_id = models.ForeignKey(Artiste, on_delete=models.CASCADE) 
+    
+
+    def __str__(self):
+        return self.title
+
+
+class Lyric(models.Model):
+    content = models.CharField(max_length=400)
+    song_id = models.ForeignKey(Song, on_delete=models.CASCADE) 
+
+
+    def __str__(self):
+        return self.song_id
+
--- songcrud/settings.py
+++ songcrud/settings.py
@@ -37,7 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'musicapp'
+
+    #3rd Party.
+    #'django_extensions',
+    'rest_framework',
+
+    #Local Apps
+    'musicapp',
+    'api.apps.ApiConfig',
+    
 ]
 
 MIDDLEWARE = [
@@ -122,3 +130,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK ={
+    "DEFAULT_AUTHENTICATION_CLASSES":(
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.BasicAuthentication"
+    )
+}
